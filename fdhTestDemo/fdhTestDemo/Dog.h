b5734b9ce46f8ed247cd03492a40abc1767e0d9e
FuDongHai <296322762@qq.com>
2017-05-31 12:05:31 +0800

    创建了一个新的类

---
+++ fdhTestDemo/fdhTestDemo/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  fdhTestDemo
+//
+//  Created by EaseMob on 17/5/31.
+//  Copyright © 2017年 EaseMob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ fdhTestDemo/fdhTestDemo/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  fdhTestDemo
+//
+//  Created by EaseMob on 17/5/31.
+//  Copyright © 2017年 EaseMob. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
