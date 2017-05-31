ea32c14d22e652cdf08859d381fc75db2c1bae8d
FuDongHai <296322762@qq.com>
2017-05-31 12:02:21 +0800

    初始化项目

---
+++ fdhTestDemo/fdhTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fdhTestDemo
+//
+//  Created by EaseMob on 17/5/31.
+//  Copyright © 2017年 EaseMob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fdhTestDemo/fdhTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fdhTestDemo
+//
+//  Created by EaseMob on 17/5/31.
+//  Copyright © 2017年 EaseMob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
